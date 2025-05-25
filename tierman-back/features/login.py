d455018002cd6f48d22292b901ec73ee11271b25
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2025-05-24 21:58:17 -0300

    add sqlite + sqlalchemy stuff :)

--- tierman-back/features/login.py
+++ tierman-back/features/login.py
@@ -1,25 +1,25 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
-from typing import List
-
-router = APIRouter(
-    prefix="/users",
-)
-
-class User(BaseModel):
-    username: str
-    password: str
-
-bob = User(username="Rob", password="admin123")
-
-@router.get("/", response_model=List[User])
-def get_all():
-    return [bob]
-
-@router.get("/rob", response_model=User)
-def get_rob():
-    return bob
-
-@router.post("/")
-def signUp():
-    return bob
+#from fastapi import APIRouter
+#from pydantic import BaseModel
+#from typing import List
+#
+#router = APIRouter(
+#    prefix="/users",
+#)
+#
+#class User(BaseModel):
+#    username: str
+#    password: str
+#
+#bob = User(username="Rob", password="admin123")
+#
+#@router.get("/", response_model=List[User])
+#def get_all():
+#    return [bob]
+#
+#@router.get("/rob", response_model=User)
+#def get_rob():
+#    return bob
+#
+#@router.post("/")
+#def signUp():
+#    return bob
