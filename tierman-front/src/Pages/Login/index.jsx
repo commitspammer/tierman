060797a20fc0ec730baa0f893491b724a5d0c628
Alexandre Guimarães <guimaraes.aco@gmail.com>
2025-05-27 21:06:47 -0300

    feat: implement authentication flow with login and registration, add protected routes

--- tierman-front/src/Pages/Login/index.jsx
+++ tierman-front/src/Pages/Login/index.jsx
@@ -1,15 +1,68 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Bounce, toast } from "react-toastify";
+import { api } from "../../Services/api";
 
 export default function Login() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    username: "",
+    login: "",
     password: "",
   });
 
   const handleChange = (event) => {
     const { id, value } = event.target;
     setFormData((prevState) => ({ ...prevState, [id]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    api
+      .post("/users/login", {
+        ...formData,
+      })
+      .then((response) => {
+        if (response.status === 201 || response.status === 200) {
+          toast.success("Usuário logado com sucesso!", {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+            onClose: () => {
+              setFormData({
+                login: "",
+                password: "",
+              });
+
+              document.cookie = `jwt=${response.data.jwt}; path=/; max-age=${
+                60 * 60 * 24
+              }`;
+
+              navigate("/");
+            },
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao entrar:", error);
+        toast.error(error, {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+      });
   };
 
   return (
@@ -19,16 +72,16 @@ export default function Login() {
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="username"
+            htmlFor="login"
           >
             Usuário
           </label>
           <input
             type="text"
-            id="username"
+            id="login"
             placeholder="Digite seu usuário"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={formData.username}
+            value={formData.login}
             onChange={handleChange}
           />
         </div>
@@ -51,6 +104,7 @@ export default function Login() {
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+          onClick={handleSubmit}
         >
           Entrar
         </button>
--- tierman-front/src/Pages/Register/index.jsx
+++ tierman-front/src/Pages/Register/index.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Bounce, toast } from "react-toastify";
+import { api } from "../../Services/api";
 
 export default function Register() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
+    email: "",
     username: "",
     password: "",
     confirmPassword: "",
@@ -12,11 +17,102 @@ export default function Register() {
     setFormData((prevState) => ({ ...prevState, [id]: value }));
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { username, email, password, confirmPassword } = formData;
+
+    if (!username || !email || !password || !confirmPassword) {
+      toast.error("Por favor, preencha todos os campos", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+      return;
+    } else if (password !== confirmPassword) {
+      toast.error("As senhas não coincidem", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    }
+
+    try {
+      const response = await api.post("/users", {
+        username,
+        email,
+        password,
+      });
+      if (response.status === 201 || response.status === 200) {
+        toast.success("Usuário cadastrado com sucesso", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+          onClose: () => {
+            setFormData({
+              email: "",
+              username: "",
+              password: "",
+              confirmPassword: "",
+            });
+
+            navigate("/login");
+          },
+        });
+      }
+    } catch (error) {
+      console.error("Erro ao cadastrar usuário:", error);
+      toast.error(error, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-10/12 bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">Cadastre-se</h1>
       <form className="bg-white p-6 rounded shadow-md w-96">
         <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Digite seu usuário"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={formData.email}
+            onChange={handleChange}
+          />
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor="username"
@@ -67,6 +163,7 @@ export default function Register() {
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+          onClick={handleSubmit}
         >
           Enviar
         </button>
